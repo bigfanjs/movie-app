5ea3ff8bba49fa98a39cecc86ca8c1b0ebddf74f
Adel Jojo <bigfanjs@gmail.com>
2017-05-01 22:36:23 +0100

    updating the movie app.js file

--- src/app.js
+++ src/app.js
@@ -1,5 +1,53 @@
 import React from 'react';
+import {BrowserRouter as Router, Route, Redirect} from 'react-router-dom';
+
+import asyncComponent from './components/async-component';
+
+import './app.scss';
+
+const
+  Movies = asyncComponent(() => (
+    import('./scenes/movies/scenes/movie-list')
+      .then(module => module.default)
+  )),
+  Watch = asyncComponent(() => (
+    import('./scenes/movies/scenes/movie-watch')
+      .then(module => module.default)
+  )),
+  Edit = asyncComponent(() => (
+    import('./scenes/admin/scenes/movie-edit')
+      .then(module => module.default)
+  )),
+  Dashboard = asyncComponent(() => (
+    import('./scenes/admin/scenes/dashboard')
+      .then(module => module.default)
+  )),
+  Login = asyncComponent(() => (
+    import('./scenes/admin/scenes/login')
+      .then(module => module.default)
+  ));
+
+const Admin = asyncComponent(() => (
+  fetch('/session')
+    .then(admin => () => {
+      const path = `/admin/${admin ? 'dashboard' : 'login'}`;
+      <Redirect to={path} />;
+    })
+));
 
 export default function () {
-  return <h1>Hello World</h1>;
+  return (
+    <Router>
+      <div>
+        <Route exact path="/" render={() => <Redirect to="/movies" />} />
+        <Route exact path="/movies" component={Movies} />
+        <Route path="/movies/watch" component={Watch} />
+        <Route exact path="/admin" component={Admin} />
+        <Route path="/admin/dashboard" component={Dashboard} />
+        <Route path="/admin/dashboard/:id" component={Edit} />
+        <Route path="/admin/new" component={Edit} />
+        <Route path="/admin/login" component={Login} />
+      </div>
+    </Router>
+  );
 }
