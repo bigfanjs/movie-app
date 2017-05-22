4bf2f9b52959187d531206600fd5aaea689ae25f
Adel Jojo <bigfanjs@gmail.com>
2017-05-22 11:55:34 +0000

    feat: adding tv shows routes

--- src/app.js
+++ src/app.js
@@ -1,34 +1,42 @@
-import React from 'react';
-import {BrowserRouter as Router, Route, Redirect} from 'react-router-dom';
+import React from "react";
+import {BrowserRouter as Router, Route, Redirect} from "react-router-dom";
 
-import asyncComponent from './components/async-component';
+import asyncComponent from "./components/async-component";
 
-import './app.scss';
+import "./app.scss";
 
 const
   Movies = asyncComponent(() => (
-    import('./scenes/movies/scenes/movie-list')
+    import("./scenes/movies/scenes/movie-list")
       .then(module => module.default)
   )),
   Watch = asyncComponent(() => (
-    import('./scenes/movies/scenes/movie-watch')
+    import("./scenes/movies/scenes/movie-watch")
       .then(module => module.default)
   )),
   Edit = asyncComponent(() => (
-    import('./scenes/admin/scenes/movie-edit')
+    import("./scenes/admin/scenes/movie-edit")
+      .then(module => module.default)
+  )),
+  Shows = asyncComponent(() => (
+    import("./scenes/shows/scenes/show-list")
+      .then(module => module.default)
+  )),
+  Episode = asyncComponent(() => (
+    import("./scenes/shows/scenes/episode-watch")
       .then(module => module.default)
   )),
   Dashboard = asyncComponent(() => (
-    import('./scenes/admin/scenes/dashboard')
+    import("./scenes/admin/scenes/dashboard")
       .then(module => module.default)
   )),
   Login = asyncComponent(() => (
-    import('./scenes/admin/scenes/login')
+    import("./scenes/admin/scenes/login")
       .then(module => module.default)
   ));
 
 const Admin = asyncComponent(() => (
-  fetch('/session')
+  fetch("/session")
     .then((admin) => admin.json())
     .then((json) => <Redirect to="/admin/dashboard" {...json} />)
     .catch(() => <Redirect to="/admin/login" />)
@@ -41,11 +49,15 @@ export default function () {
       <div>
         <Route exact path="/" render={() => <Redirect to="/movies" />} />
         <Route exact path="/movies" component={Movies} />
+        <Route path="/shows/" component={Shows} />
         <Route path="/movies/watch/:id" component={Watch} />
+        <Route path="/shows/seasons/:id/episodes/:id" component={Episode} />
         <Route exact path="/admin" component={Admin} />
         <Route path="/admin/dashboard" component={Dashboard} />
-        <Route path="/admin/dashboard/:id" component={Edit} />
-        <Route path="/movies/new" component={Edit} />
+        <Route path="/admin/dashboard/movies/edit/:id" component={Edit} />
+        <Route path="/admin/dashboard/shows/edit/:id" component={Edit} />
+        <Route path="/admin/dashboard/movies/new" component={Edit} />
+        <Route path="/admin/dashboard/shows/new" component={Edit} />
         <Route path="/admin/login" component={Login} />
       </div>
     </Router>
