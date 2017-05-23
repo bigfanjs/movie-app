3789dbea4e57a291508eb6116a2aa12a24e93c6a
Adel Jojo <bigfanjs@gmail.com>
2017-05-23 16:03:33 +0000

    setting up the app scenes with plaseholder components

---
+++ src/scenes/admin/scenes/login/index.js
@@ -0,0 +1,6 @@
+// placeholder component.
+import React from 'react';
+
+export default function () {
+  return <h1>Login</h1>;
+}
---
+++ src/scenes/admin/scenes/movie-edit/index.js
@@ -0,0 +1,6 @@
+// placeholder component.
+import React from 'react';
+
+export default function () {
+  return <h1>Edit/Create</h1>;
+}
