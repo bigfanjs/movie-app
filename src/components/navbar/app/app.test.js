78703b963bef4f38a5b687f9e0371a4113e7a142
Adel Jojo <bigfanjs@gmail.com>
2017-05-24 13:00:17 +0000

    fix: imports App as a default export

--- src/components/navbar/app/app.test.js
+++ src/components/navbar/app/app.test.js
@@ -1,7 +1,7 @@
 import React from "react";
 import renderer from "react-test-renderer";
 
-import {App} from "./app";
+import App from "./app";
 
 test("<App />", function () {
   const tree = renderer.create(<App />).toJSON();
