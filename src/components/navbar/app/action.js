102aed0df199928b69e5b92d965e542b438a0459
Adel Jojo <bigfanjs@gmail.com>
2017-05-25 11:58:36 +0000

    feat: updating the navbar action.js file

--- src/components/navbar/app/action.js
+++ src/components/navbar/app/action.js
@@ -1,6 +1,13 @@
-export default function (panel) {
+export function openPanel(panel) {
   return {
-    type: "NAVBAR_TOGGLE",
+    type: "OPEN_PANEL",
     panel: panel
+  };
+}
+
+export function closePanel() {
+  return {
+    type: "CLOSE_PANEL",
+    panel: null
   };
 }
