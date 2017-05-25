e5014ae1e3152a0a1632f5d8673ce5450fff4b6c
Adel Jojo <bigfanjs@gmail.com>
2017-05-25 12:09:24 +0000

    fix: forces the the reducers to get replaced when an update happen

--- src/main.js
+++ src/main.js
@@ -1,10 +1,10 @@
-import React from 'react';
-import ReactDom from 'react-dom';
-import {AppContainer} from 'react-hot-loader';
-import {Provider} from 'react-redux';
-import {createStore} from 'redux';
-import movieApp from './reducers';
-import App from './app';
+import React from "react";
+import ReactDom from "react-dom";
+import {AppContainer} from "react-hot-loader";
+import {Provider} from "react-redux";
+import {createStore} from "redux";
+import movieApp from "./reducers";
+import App from "./app";
 
 const store = createStore(movieApp);
 
@@ -15,12 +15,18 @@ const render = Component => (
         <Component />
       </Provider>
     </AppContainer>,
-    document.getElementById('root')
+    document.getElementById("root")
   )
 );
 
 render(App);
 
 if (module.hot) {
-  module.hot.accept('./app', render.bind(null, App));
+  module.hot.accept("./app", render.bind(null, App));
+  module.hot.accept("./reducers", function () {
+    import("./reducers") // eslint-disable-line
+      .then(nextReducer => {
+        store.replaceReducer(nextReducer.default);
+      });
+  });
 }
