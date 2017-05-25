b8ba3154d5d371e7f0321830dc68ba681c4cc2f7
Adel Jojo <bigfanjs@gmail.com>
2017-05-25 11:55:59 +0000

    feat: makes code splitting compatible with hot reloading by replacing the async-component with the bundle-component

--- src/app.js
+++ src/app.js
@@ -2,38 +2,34 @@ import React from "react";
 import {BrowserRouter as Router, Route, Redirect} from "react-router-dom";
 
 import asyncComponent from "./components/async-component";
+import Bundle from "./components/bundle";
+
+import movieList from "bundle-loader?lazy!./scenes/movies/scenes/movie-list";
+import movieWatch from "bundle-loader?lazy!./scenes/movies/scenes/movie-watch";
+import movieEdit from "bundle-loader?lazy!./scenes/admin/scenes/movie-edit";
+
+import showList from "bundle-loader?lazy!./scenes/shows/scenes/show-list";
+import episode from "bundle-loader?lazy!./scenes/shows/scenes/episode-watch";
+import showEdit from "bundle-loader?lazy!./scenes/shows/scenes/show-edit";
+
+import dashboard from "bundle-loader?lazy!./scenes/admin/scenes/dashboard";
+import login from "bundle-loader?lazy!./scenes/admin/scenes/login";
 
 import "./app.scss";
 
 const
-  Movies = asyncComponent(() => (
-    import("./scenes/movies/scenes/movie-list")
-      .then(module => module.default)
-  )),
-  Watch = asyncComponent(() => (
-    import("./scenes/movies/scenes/movie-watch")
-      .then(module => module.default)
-  )),
-  Edit = asyncComponent(() => (
-    import("./scenes/admin/scenes/movie-edit")
-      .then(module => module.default)
-  )),
-  Shows = asyncComponent(() => (
-    import("./scenes/shows/scenes/show-list")
-      .then(module => module.default)
-  )),
-  Episode = asyncComponent(() => (
-    import("./scenes/shows/scenes/episode-watch")
-      .then(module => module.default)
-  )),
-  Dashboard = asyncComponent(() => (
-    import("./scenes/admin/scenes/dashboard")
-      .then(module => module.default)
-  )),
-  Login = asyncComponent(() => (
-    import("./scenes/admin/scenes/login")
-      .then(module => module.default)
-  ));
+  Movies = (props) => <Bundle load={movieList} {...props} />,
+  Watch = (props) => <Bundle load={movieWatch} {...props} />,
+  MovieEdit = (props) => <Bundle load={movieEdit} {...props} />;
+
+const
+  Shows = (props) => <Bundle load={showList} {...props} />,
+  Episode = (props) => <Bundle load={episode} {...props} />,
+  ShowEdit = (props) => <Bundle load={showEdit} {...props} />;
+
+const
+  Dashboard = (props) => <Bundle load={dashboard} {...props} />,
+  Login = (props) => <Bundle load={login} {...props} />;
 
 const Admin = asyncComponent(() => (
   fetch("/session")
@@ -48,16 +44,19 @@ export default function () {
     <Router>
       <div>
         <Route exact path="/" render={() => <Redirect to="/movies" />} />
+
         <Route exact path="/movies" component={Movies} />
-        <Route path="/shows/" component={Shows} />
         <Route path="/movies/watch/:id" component={Watch} />
+        <Route path="/movies/edit/:id" component={MovieEdit} />
+        <Route path="/movies/new" component={MovieEdit} />
+
+        <Route exact path="/shows" component={Shows} />
         <Route path="/shows/seasons/:id/episodes/:id" component={Episode} />
+        <Route path="/shows/edit/:id" component={ShowEdit} />
+        <Route path="/shows/new" component={ShowEdit} />
+
         <Route exact path="/admin" component={Admin} />
         <Route path="/admin/dashboard" component={Dashboard} />
-        <Route path="/admin/dashboard/movies/edit/:id" component={Edit} />
-        <Route path="/admin/dashboard/shows/edit/:id" component={Edit} />
-        <Route path="/admin/dashboard/movies/new" component={Edit} />
-        <Route path="/admin/dashboard/shows/new" component={Edit} />
         <Route path="/admin/login" component={Login} />
       </div>
     </Router>
