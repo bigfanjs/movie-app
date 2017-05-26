8acd5e4d46501ce47662e146e6b1185105a2b0bb
Adel Jojo <bigfanjs@gmail.com>
2017-05-26 11:05:36 +0000

    feat: adds a li wrapper around the svg element

--- src/components/navbar/menu-icon/menu-icon.js
+++ src/components/navbar/menu-icon/menu-icon.js
@@ -1,15 +1,13 @@
 import React from "react";
 
-import classNames from "./menu-icon.scss";
-
 export default function ({onOpen}) {
   return (
-    <div className={classNames.menuIcon} onClick={onOpen}>
+    <li onClick={onOpen}>
       <svg width="30" height="23">
-        <rect x="0" y="0" width="30" height="3" />
-        <rect x="0" y="10" width="30" height="3" />
-        <rect x="0" y="20" width="30" height="3" />
+        <rect x="0" y="0" width="30" height="3" fill="#fff" />
+        <rect x="0" y="10" width="30" height="3" fill="#fff" />
+        <rect x="0" y="20" width="30" height="3" fill="#fff" />
       </svg>
-    </div>
+    </li>
   );
 }
