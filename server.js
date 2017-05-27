e653c5a7d81703ea1ce49540abe742d928865e5e
Adel Jojo <bigfanjs@gmail.com>
2017-05-27 18:41:42 +0000

    feat: updates the server.js file

--- server.js
+++ server.js
@@ -1,42 +1,42 @@
 /*global __dirname*/
 
 const
-  path = require('path'),
-  webpack = require('webpack'),
-  logger = require('morgan'),
-  express = require('express'),
-  bodyPasrer = require('body-parser'),
-  session = require('express-session'),
-  cookieParser = require('cookie-parser'),
-  methodOverride = require('method-override'),
-  multer = require('multer'),
-  config = require('./webpack.config');
+  path = require("path"),
+  webpack = require("webpack"),
+  logger = require("morgan"),
+  express = require("express"),
+  bodyPasrer = require("body-parser"),
+  session = require("express-session"),
+  cookieParser = require("cookie-parser"),
+  methodOverride = require("method-override"),
+  multer = require("multer"),
+  config = require("./webpack.config");
 
 const
   join = path.join,
   app = express(),
-  upload = multer({dest: 'tmp/'}),
+  upload = multer({dest: "tmp/"}),
   compiler = webpack(config);
 
-app.set('covers', join(__dirname, './public/images/covers/'));
-app.set('avatars', join(__dirname, './public/images/avatars/'));
+app.set("covers", join(__dirname, "./public/images/covers/"));
+app.set("avatars", join(__dirname, "./public/images/avatars/"));
 
 // require routes:
 const
-  movies = require('./routes/movies'),
-  login = require('./routes/login');
+  movies = require("./routes/movies"),
+  login = require("./routes/login");
 
 // require middleware:
-const admin = require('./lib/middleware/admin');
+const admin = require("./lib/middleware/admin");
 
 const
-  webpackDevMiddleware = require('webpack-dev-middleware'),
-  webpackHotMiddleware = require('webpack-hot-middleware');
+  webpackDevMiddleware = require("webpack-dev-middleware"),
+  webpackHotMiddleware = require("webpack-hot-middleware");
 
 app.use(webpackDevMiddleware(compiler, {
   publicPath: config.output.publicPath,
   hot: true,
-  filename: 'bundle.js',
+  filename: "bundle.js",
   stats: {
     colors: true
   },
@@ -51,49 +51,49 @@ const isAuth = function (req, res, next) {
     return next();
   }
 
-  res.status(401).end('Access Denied');
+  res.status(401).end("Access Denied");
 };
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(bodyPasrer.json());
-app.use(cookieParser('some secret code'));
+app.use(cookieParser("some secret code"));
 app.use(session({
   resave: false,
   saveUninitialized: false,
-  secret: 'some secret code'
+  secret: "some secret code"
 }));
 app.use(methodOverride());
-app.use(express.static(path.join(__dirname, './dist')));
+app.use(express.static(path.join(__dirname, "./dist")));
 app.use(admin());
 
-app.post('/admin/login', login.submit);
+app.post("/admin/login", login.submit);
 
-app.get('/session', isAuth, function (req, res) {
+app.get("/session", isAuth, function (req, res) {
   res.send(200, res.admin);
 });
 
-app.get('/api/movies/', movies.showMovies);
-app.post('/api/movies/', isAuth, movies.createMovie);
-app.get('/api/movies/count', movies.count);
-app.get('/api/movies/genres', movies.genres);
-app.get('/api/movies/:id', movies.viewMovie);
-app.put('/api/movies/:id', isAuth, movies.updateMovie);
-app.delete('/api/movies/:id', isAuth, movies.deleteMovie);
+app.get("/api/movies/", movies.showMovies);
+app.post("/api/movies/", isAuth, movies.createMovie);
+app.get("/api/movies/count", movies.count);
+app.get("/api/movies/genres", movies.genres);
+app.get("/api/movies/:id", movies.viewMovie);
+app.put("/api/movies/:id", isAuth, movies.updateMovie);
+app.delete("/api/movies/:id", isAuth, movies.deleteMovie);
 app.post(
-  '/api/movies/:id/cover',
+  "/api/movies/:id/cover",
   isAuth,
-  upload.single('cover'),
-  movies.uploadCover.bind(null, app.get('covers'))
+  upload.single("cover"),
+  movies.uploadCover.bind(null, app.get("covers"))
 );
 app.post(
-  '/api/movies/:id/avatars',
+  "/api/movies/:id/avatars",
   isAuth,
-  upload.array('avatars'),
-  movies.uploadAvatars.bind(null, app.get('avatars'))
+  upload.array("avatars"),
+  movies.uploadAvatars.bind(null, app.get("avatars"))
 );
 
-app.get('*', function(req, res) {
-  res.sendFile(path.resolve(__dirname, 'dist', 'app.html'));
+app.get("*", function(req, res) {
+  res.sendFile(path.resolve(__dirname, "dist", "app.html"));
 });
 
 app.listen(3000, function(err) {
@@ -101,5 +101,5 @@ app.listen(3000, function(err) {
     return console.error(err); // eslint-disable-line
   }
 
-  console.log('Listening at http://localhost:3000/'); // eslint-disable-line
+  console.log("Listening at http://localhost:3000/"); // eslint-disable-line
 });
