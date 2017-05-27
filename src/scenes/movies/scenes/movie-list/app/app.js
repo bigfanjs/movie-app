123e84c676ff9ecfc663d76ff8d17c90c457e8e4
Adel Jojo <bigfanjs@gmail.com>
2017-05-27 18:55:30 +0000

    feat: removes the movie- filter component from the movie-list

--- src/scenes/movies/scenes/movie-list/app/app.js
+++ src/scenes/movies/scenes/movie-list/app/app.js
@@ -1,27 +1,16 @@
 import React from "react";
-import {connect} from "react-redux";
 
 import Navbar from "../../../../../components/navbar";
 
 import MovieList from "../movie-list";
-import MovieFilter from "../movie-filter";
-
-import movieFilter from "./action";
 
 import classNames from "./app.scss";
 
-export default connect()(
-  function App({movies, dispatch}) {
-    const handleFilter = function (movies) {
-      dispatch(movieFilter(movies));
-    };
-
-    return (
-      <div className={classNames.movieList}>
-        <Navbar />
-        <MovieFilter onfilter={handleFilter} />
-        <MovieList movies={movies} />
-      </div>
-    );
-  }
-);
+export default function () {
+  return (
+    <div className={classNames.movieList}>
+      <Navbar />
+      <MovieList />
+    </div>
+  );
+}
