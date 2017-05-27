c4af53a1de6a79b62834a2f1121b54d99a474e21
Adel Jojo <bigfanjs@gmail.com>
2017-05-27 18:56:45 +0000

    feat: updates the movies reducer.js

--- src/scenes/movies/scenes/movie-list/app/reducer.js
+++ src/scenes/movies/scenes/movie-list/app/reducer.js
@@ -1,3 +1,10 @@
-export default function (state = []) {
-  return state;
+export default function (state = [], action) {
+  switch (action.type) {
+    case "ADD_MOVIES":
+      return [...state, ...action.movies];
+    case "DELETE_MOVIE":
+      return state.filter((movie) => movie.id === action.id);
+    default:
+      return state;
+  }
 }
