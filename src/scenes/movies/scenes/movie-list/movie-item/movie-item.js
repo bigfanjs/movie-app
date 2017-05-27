78504910f68c1d62c44ca14a66acb93d597dcd52
Adel Jojo <bigfanjs@gmail.com>
2017-05-27 18:59:31 +0000

    feat: uses placeholde.it web service for movies covers placeholders

--- src/scenes/movies/scenes/movie-list/movie-item/movie-item.js
+++ src/scenes/movies/scenes/movie-list/movie-item/movie-item.js
@@ -5,12 +5,14 @@ import {movieItem} from "./movie-item.scss";
 export default function ({title, releaseYear, rating, cover}) {
   return (
     <li className={movieItem}>
-      <img src={cover.path} />
-      <span>
-        <h2>{title}</h2>
-        <h4>{releaseYear}</h4>
-        <h4>{rating}</h4>
-      </span>
+      <img src="http://placehold.it/206x300" />
+      <div>
+        <span>
+          <h2>{title}</h2>
+          <h4>{releaseYear}</h4>
+          <h4>{rating}</h4>
+        </span>
+      </div>
     </li>
   );
 }
