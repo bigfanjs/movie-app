4acaa791763fdb9ea6c11304fd81fe8923abd072
Adel Jojo <bigfanjs@gmail.com>
2017-05-29 05:16:10 +0000

    feat: opens menu panel when menu icon clicked

--- src/components/navbar/menu-icon/menu-icon.js
+++ src/components/navbar/menu-icon/menu-icon.js
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function ({onOpen}) {
   return (
-    <li onClick={onOpen}>
+    <li onClick={onOpen.bind(null, "menu")}>
       <svg width="30" height="23">
         <rect x="0" y="0" width="30" height="3" fill="#fff" />
         <rect x="0" y="10" width="30" height="3" fill="#fff" />
